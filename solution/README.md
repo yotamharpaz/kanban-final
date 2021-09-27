8309fd8330663ac503b56891f6db5cd6ecc686b5
yotamharpaz <yotamh42@gmail.com>
2021-09-27 06:25:31 +0300

    finel fixes,css,readme

---
+++ solution/README.md
@@ -0,0 +1,22 @@
+hey guys!
+  
+  this is my kanban boeard,
+  list youre tasks and track your prograss
+  have a grate time while getting your job done with this amazing board/ 
+
+Features
+
+
+  Manage tasks with multiple boards
+  add tasks by the add button of the enter key
+  move your tasks with alt + the number task section
+  want to delete a task no problem! just hover over it and press delete
+  if you need to edit a task just tap twice and you can rewrite it 
+  
+
+
+Requirements
+
+  computer and a Browser oh and an internet conection
+  
+  
--- solution/index.js
+++ solution/index.js
@@ -70,11 +70,9 @@ function createNewTask(id,taskText = null,addToLS = true){
     }
     const list = document.getElementsByClassName(`${id}-tasks`)[0];
     const newTask = document.createElement("li");
-    list.appendChild(newTask);
+    list.replaceChildren(newTask, ...list.children);
     newTask.classList.add("task");
     newTask.appendChild(document.createTextNode(textInput));
-    newTask.classList.add("draggable");
-    newTask.setAttribute("draggable","true");
     newTask.addEventListener("dblclick",() => {
         removeItemFromLocalStorage(newTask.textContent ,id);
         newTask.contentEditable = true;
@@ -84,7 +82,6 @@ function createNewTask(id,taskText = null,addToLS = true){
             newTask.textContent ? addItemToLocalStorage(newTask.textContent ,id) : newTask.remove();
         }
     });
-
     newTask.addEventListener("mouseenter",() => mouseOverFunc(newTask));
      
     if(addToLS) addItemToLocalStorage(textInput, id);
@@ -114,6 +111,7 @@ function modifyTask(event){
     
     removeItemFromLocalStorage(task.textContent,task.closest('section').id)
     task.remove();
+    // list.children.sort((a,b) => a-b);
 }}
  
 
@@ -151,66 +149,9 @@ function createSectionElement(id) {
 
 
 function search(){
-    const searchText = document.getElementById("search").value;
-    const searchTextList = searchText.toLowerCase().split("");
-    const sections = Array.from(document.getElementsByTagName("section"));
-    sections.forEach((section)=>Array.from(section.firstChild.children).forEach((task)=>{
-        task.style.display = "none";
-        if(searchText !== null && searchText !== ""){
-            hideByFilter(task,searchTextList);}else{task.style.display = "block";}
-    })
-    )
-}
-function hideByFilter(task,searchTextList) {
+    const searchText = document.getElementById("search").value.toLowerCase();
+    const liList = document.querySelectorAll("li")
+    liList.forEach((li)=> li.hidden = !li.textContent.includes(searchText))
     
-        taskTextList = task.textContent.toLowerCase().split("");
-        let i = 0;
-        if(taskTextList.length >= searchTextList.length){
-            while (taskTextList[i] === searchTextList[i] && i <= searchTextList.length) {          
-                task.style.display = "block" ;
-                i++
-             }
-        }   
 }
 
-const draggables = document.querySelectorAll('.draggable')
-const sections = document.querySelectorAll('section')
-console.log(draggables);
-
-draggables.forEach(draggable => {
-  draggable.addEventListener('dragstart', () => {
-    draggable.classList.add('dragging')
-  })
-
-  draggable.addEventListener('dragend', () => {
-    draggable.classList.remove('dragging')
-  })
-})
-
-sections.forEach(section => {
-  section.addEventListener('dragover', e => {
-    e.preventDefault()
-    const ul = document.getElementsByTagName("ul");
-    const afterElement = getDragAfterElement(ul, e.clientY);
-    const draggable = document.querySelector('.dragging');
-    if (afterElement == null) {
-      section.appendChild(draggable);
-    } else {
-      section.insertBefore(draggable, afterElement);
-    }
-  })
-})
-
-function getDragAfterElement(section, y,) {
-  const draggableElements = [...section.querySelectorAll('.draggable:not(.dragging)')];
-
-  return draggableElements.reduce((closest, child) => {
-    const box = child.getBoundingClientRect();
-    const offset = y - box.top - box.height / 2;
-    if (offset < 0 && offset > closest.offset) {
-      return { offset: offset, element: child }
-    } else {
-      return closest
-    }
-  }, { offset: Number.NEGATIVE_INFINITY }).element
-}
